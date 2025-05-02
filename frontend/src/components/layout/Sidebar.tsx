5ed917b2193dc62a40aceaf874d90e39966814e6
samanhappy <samanhappy@gmail.com>
2025-05-02 15:10:50 +0800

    feat: add log management features including log viewing, filtering, and streaming

--- frontend/src/components/layout/Sidebar.tsx
+++ frontend/src/components/layout/Sidebar.tsx
@@ -56,6 +56,15 @@ const Sidebar: React.FC<SidebarProps> = ({ collapsed }) => {
       ),
     },
     {
+      path: '/logs',
+      label: t('nav.logs'),
+      icon: (
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+          <path fillRule="evenodd" d="M5 3a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2V5a2 2 0 00-2-2H5zm0 2h10v7h-2l-1 2H8l-1-2H5V5z" clipRule="evenodd" />
+        </svg>
+      ),
+    },
+    {
       path: '/settings',
       label: t('nav.settings'),
       icon: (
